f54095704c5f07fe24810f1c3b1ca1a5cd4bd359
Ben DePew <ben@agileworks.io>
2019-05-15 20:45:39 -0400

    Initial Commit

---
+++ cloudi.io/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  cloudi.io
+//
+//  Created by Depew, Ben (NonEmp) on 5/15/19.
+//  Copyright © 2019 Depew, Ben (NonEmp). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
